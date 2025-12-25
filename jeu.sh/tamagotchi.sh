a5086f93dbaf938c48b1c509984d6efe41c31708
Eliott <eliott_gh@proton.me>
2025-12-25 14:21:21 +0100

    Choix nom

--- jeu.sh/tamagotchi.sh
+++ jeu.sh/tamagotchi.sh
@@ -1,1 +1,17 @@
-#!bin/bash
+#!/bin/bash
+
+
+read -p "Quel est le nom de ta créature ? " nom
+
+while [[ -z "$nom" ]]; do
+
+echo "Tu n'as pas choisi de nom pour ta créature, recommence banane ! "
+echo
+sleep 2
+read -p "Quel est le nom de ta créature ? " nom
+
+done
+
+
+
+
