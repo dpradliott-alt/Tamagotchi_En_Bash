bdd12a11fd0f91fbd7c8c37d0f5f1403e3d254e7
dpradliott-alt <dpradliott@gmail.com>
2025-12-25 15:50:02 +0100

    Fichier stats ajouté

---
+++ jeu.sh/Fonctions/stats.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+faim=0
+age=0
+bonheur=5
+sale=0
+vivant=1
+energie=5
+force=1
+vitesse=1
+intelligence=1
+
+afficher_stats_oeuf() {
+
+echo
+    echo "🐣 $nom"
+    echo "Âge      : $age"
+    echo "Faim     : $faim"
+    echo "Énergie  : $energie"
+    echo "Bonheur  : $bonheur"
+    echo "Force : $force"
+	echo "Vitesse : $vitesse"
+    echo "Intelligence : $intelligence"
+    echo
+}
